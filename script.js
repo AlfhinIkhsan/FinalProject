69edb63e329ab57f5bfb8c529fa34e05081cc293
AlfhinIkhsan <alfhinikhsan74@gmail.com>
2023-09-22 07:51:41 +0700

    commit message

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// JavaScript for Dark Mode Toggle
+
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 const body = document.body;
 
@@ -7,11 +7,11 @@ darkModeToggle.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
 });
 
-// Ambil elemen tombol hamburger dan menu mobile
+
 const hamburgerButton = document.querySelector('.hamburger-button');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-// Tambahkan event listener untuk mengganti kelas 'active' saat tombol hamburger diklik
+
 hamburgerButton.addEventListener('click', () => {
     mobileMenu.classList.toggle('active');
 });
